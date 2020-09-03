45b33308fd1900ea948ac7b4e2fa9ae454dadbf5
Nick Pesce <nickpesce22@gmail.com>
2020-09-02 22:46:51 -0400

    Ready for pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lit-web',
-    version='0.0',
+    version='0.0.0',
     description='LIT web server',
     packages=['litweb'],
     include_package_data=True,
